4282869417cd3702c53e1c3ab88363c17c64e7e4
John Sloan <jsloan256@gmail.com>
2016-10-31 22:09:12 +0000

    Added basic Pi gpio control

--- hallow_client.py
+++ hallow_client.py
@@ -1,26 +1,42 @@
 import socket
 import sys
+import time
+import RPi.GPIO as gpio
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+## Create a TCP/IP socket
+#sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#
+## Connect the socket to the port on the server given by the caller
+#server_address = ('tweedle.hallow.local', 10000)
+#print >> sys.stderr, 'connecting to %s port %s' % server_address
+#sock.connect(server_address)
+#
+#try:
+#    
+#  message = 'This is the message.  It will be repeated.'
+#  print >>sys.stderr, 'sending "%s"' % message
+#  sock.sendall(message)
+#
+#  amount_received = 0
+#  amount_expected = len(message)
+#  while amount_received < amount_expected:
+#    data = sock.recv(16)
+#    amount_received += len(data)
+#    print >>sys.stderr, 'received "%s"' % data
+#
+#finally:
+#  sock.close()
 
-# Connect the socket to the port on the server given by the caller
-server_address = ('tweedle.hallow.local', 10000)
-print >> sys.stderr, 'connecting to %s port %s' % server_address
-sock.connect(server_address)
+# Use Rapberry Pi BOARD pin numbers
+gpio.setmode(gpio.BOARD)
+gpio.setwarnings(False) 
 
-try:
-    
-  message = 'This is the message.  It will be repeated.'
-  print >>sys.stderr, 'sending "%s"' % message
-  sock.sendall(message)
+gpio.setup(11, gpio.OUT)
+gpio.setup(11, gpio.OUT)
+gpio.setup(12, gpio.OUT)
 
-  amount_received = 0
-  amount_expected = len(message)
-  while amount_received < amount_expected:
-    data = sock.recv(16)
-    amount_received += len(data)
-    print >>sys.stderr, 'received "%s"' % data
-
-finally:
-  sock.close()
+gpio.output(11, gpio.HIGH)
+gpio.output(12, gpio.HIGH)
+time.sleep(1)
+gpio.output(11, gpio.LOW)
+gpio.output(12, gpio.LOW)
