3a673e21007d46a38ffd08097fe3790e75f0809e
John Sloan <jsloan256@gmail.com>
2016-10-31 21:33:57 +0000

    wip

--- hallow_server.py
+++ hallow_server.py
@@ -6,7 +6,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Bind the socket to the address given on the command line
 server_address = ('tweedle.hallow.local', 10000)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
+print >> sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 sock.listen(1)
 
