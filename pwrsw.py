fc2558168e7b6474f51c6e23c7c4cc78d1278f1f
John Sloan <jsloan256@gmail.com>
2016-11-17 19:14:05 +0000

    Added argument control

--- pwrsw.py
+++ pwrsw.py
@@ -4,41 +4,40 @@ import RPi.GPIO as gpio
 import argparse
 
 parser = argparse.ArgumentParser(description='Set PS switch state')
-parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                    help='an integer defining the PS state')
-parser.add_argument('--state', dest='state', action='store_const',
-                    const=sum, default=max,
-                    help='Set the PS switch state (default: 2, pulse)')
+parser.add_argument('--state', dest='state',
+                    default='cycle',
+                    help='Set the PS switch state (cycle, on, off)')
 
 args = parser.parse_args()
-#print args.state(args.integers)
-
-
 
 # Use Rapberry Pi BOARD pin numbers
 gpio.setmode(gpio.BOARD)
 gpio.setwarnings(False) 
-
-gpio.setup(11, gpio.OUT)
 gpio.setup(11, gpio.OUT)
 gpio.setup(12, gpio.OUT)
 
-# OFF
-gpio.output(11, gpio.HIGH)
-gpio.output(12, gpio.HIGH)
+# Operate the IoT power switch based on the 'state' argument
+if (args.state == 'cycle'):
 
-time.sleep(3)
+  # OFF
+  gpio.output(11, gpio.HIGH)
+  gpio.output(12, gpio.HIGH)
 
-# ON
-gpio.output(11, gpio.LOW)
-gpio.output(12, gpio.LOW)
+  time.sleep(3)
 
-#while (1):
-#  time.sleep(1)
-#  gpio.output(11, gpio.LOW)
-#  gpio.output(12, gpio.LOW)
+  # ON
+  gpio.output(11, gpio.LOW)
+  gpio.output(12, gpio.LOW)
 
-#  time.sleep(1)
-#  gpio.output(11, gpio.HIGH)
-#  gpio.output(12, gpio.HIGH)
+elif (args.state == 'on'):
+
+  # ON
+  gpio.output(11, gpio.LOW)
+  gpio.output(12, gpio.LOW)
+
+elif (args.state == 'off'):
+
+  # OFF
+  gpio.output(11, gpio.HIGH)
+  gpio.output(12, gpio.HIGH)
 
